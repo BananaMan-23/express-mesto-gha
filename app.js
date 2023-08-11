c8039166ef79d837046273b74c7413e8637609b9
BananaMan <suvorov.andrey755@gmail.com>
2023-08-11 21:24:09 +0300

    test1

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, Data_Bace = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log("hello");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect(Data_Bace, {
+  useNewUrlParser: true,
 });
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '5d8b8592978f8bd833ca8133' // вставьте сюда _id созданного в предыдущем пункте пользователя
+  };
+
+  next();
+});
+
+app.use('/users', require('./routes/users'));
+app.use('/cards', require('./routes/cards'));
+
+app.use('/', (req, res) => {
+  res.status(404).send({ message: 'Страница не найдена.' });
+});
+
+app.listen(PORT);
